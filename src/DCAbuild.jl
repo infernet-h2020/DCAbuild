c3475896ee95b40c2beef508873817efc9fa23b0
Andrea Pagnani <andrea.pagnani@gmail.com>
2020-06-01 18:10:42 +0200

    initial commits

--- src/DCAbuild.jl
+++ src/DCAbuild.jl
@@ -1,7 +1,7 @@
 module DCAbuild
-using AlignPotts
+#using AlignPotts
 using PlmDCA, FastaIO
-using Statistics, Printf, DelimitedFiles
+using Statistics, Printf, DelimitedFiles, Logging
 #using ExtractMacro, OffsetArrays
 #export Seq, palign
 #import Base.show
@@ -9,7 +9,17 @@ using Statistics, Printf, DelimitedFiles
 
 
 include("insertions.jl")      # all insertions related functions
-include("utils.jl")	      # i/o functions, 
+include("utils.jl")	      # i/o functions,
 include("build_model.jl")     # main script for learning a Potts model, insertion penalties and gap penalties
+
+
+function __init__()
+	if Sys.which("mafft") === nothing
+		 error("Please install mafft binaries at https://mafft.cbrc.jp/alignment/software/mafft-7.453-with-extensions-src.tgz and make the executables available to the system")
+	end
+	if Sys.which("hmmbuild") === nothing
+		error("Please install hmmer suite at http://eddylab.org/software/hmmer/hmmer.tar.gz and make the executables available to the system")
+	end
+end
 
 end #end module
